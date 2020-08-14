0a115b3dd4651f4696a2bf0351660a50e243ef3a
rgleighton1s <52300623+rgleighton1s@users.noreply.github.com>
2020-08-14 09:43:39 -0500

    Added Comic Book Detail view

--- src/ComicBookGallery2/Controllers/ComicBooksController.cs
+++ src/ComicBookGallery2/Controllers/ComicBooksController.cs
@@ -10,12 +10,7 @@ namespace ComicBookGallery2.Controllers
     {
         public ActionResult Detail()
         {
-            if (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday)
-            {
-                return Redirect("/");
-            }
-
-            return Content("Hello from the comic books controller!");
+            return View();
         }
     }
 }
